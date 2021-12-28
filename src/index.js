409a16205bc029622ad793198f3e035d2967afa5
rs-kkit0 <38030070+rs-kkit0@users.noreply.github.com>
2021-12-28 23:03:43 +0900

    button追加

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom';
-import { Game } from './game/game'
+import { Game } from './game/game';
+import Button from '@mui/material/Button';
 
 ReactDOM.render(
-    <Game />,
+    <div>
+        <Button>Hello World</Button>
+        <Game />
+    </div>,
     document.getElementById('root')
 );
