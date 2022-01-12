e852f734c9b01b30ac4eabaa112840b85c819844
rs-kkit0 <38030070+rs-kkit0@users.noreply.github.com>
2022-01-12 23:22:38 +0900

    dashboard 追加

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import ReactDOM from 'react-dom';
-import Game from './Game/Game';
-import Button from '@mui/material/Button';
+import Dashboard from './Dashboard/Dashboard';
 
 ReactDOM.render(
     <div>
-        <Button>Hello World</Button>
-        <Game />
+        <Dashboard />
     </div>,
     document.getElementById('root')
 );
